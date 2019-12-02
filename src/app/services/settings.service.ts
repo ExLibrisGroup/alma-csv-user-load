794b7bb3fc5036db36f0b08168a04eb046e12c4d
Josh Weisman <josh.m.weisman@gmail.com>
2019-12-02 12:22:48 +0200

    Cache settings service

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { FormGroup, FormArray, FormControl, AbstractControl } from '@angular/forms';
 import { Utils } from 'src/utilities';
 import { environment } from 'src/environments/environment';
@@ -12,19 +12,28 @@ import { environment } from 'src/environments/environment';
 export class SettingsService {
 
   private _settings: string;
+  private cache$: Observable<Object>;
 
   constructor( private http: HttpClient ) { }
 
-  get(): Observable<any> {
+  get settings() {
+    if (!this.cache$) 
+      this.cache$ = this.requestSettings().pipe(shareReplay());
+
+    return this.cache$;
+  }
+
+  requestSettings() {
     const url = environment.settingsService + `?user_id=joshw&app_id=exlcodeshare/test`;
     return this.http.get(url);
   }
 
   getAsFormGroup(): Observable<any> {
-    return this.get().pipe(map(settings => asFormGroup(settings)))
+    return this.settings.pipe(map(settings => asFormGroup(settings)));
   }
 
   set(settings: Object): Observable<any> {
+    this.cache$ = null;
     const url = environment.settingsService;
     return this.http.post(url, { userId: environment.userId, appId: environment.appId, settings: settings});
   }
