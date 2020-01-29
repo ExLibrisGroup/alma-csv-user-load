756c48df3fca6453d1fc8c09e7777a5a7cda13b2
Josh Weisman <josh.m.weisman@gmail.com>
2020-01-29 23:13:05 +0200

    Skip emtpy lines & update SDK version

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -59,7 +59,8 @@ export class MainComponent implements OnInit {
     this.log(this.translate.instant('Main.Parsing'));
     this.papa.parse(this.files[0], {
       header: true,
-      complete: this.parsed
+      complete: this.parsed,
+      skipEmptyLines: 'greedy'
     });
   }
 
